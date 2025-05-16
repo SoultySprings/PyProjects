73ad470c4d2483997961b5a4cd4cfa0d8cf98a47
SoultySprings <tanishqchakravarty@gmail.com>
2025-05-16 12:31:24 +0530

    commit 160525

--- Games/Bagels.py
+++ Games/Bagels.py
@@ -4,10 +4,29 @@ import random
 num_digits = 3
 max_guess = 20
 
-def getsecrectnumer:
+def main():
+    print('''The following game allows the user to deduce a three digit random integer.
+    the rules simply says that:
+    1. if a digit is in the right place - Fermi
+    2. if digit in the wrong place but exists in number - Gamma
+    3. if digit not in number - Bagels
+    ''')
+    guessing_number = getsecrectnumber()
+
+    while True:
+        print(f'You have {max_guess} guesses in total.)')
+        print('''The number has been guessed''')
+        guess = 1
+        while guess <= max_guess :
+
+
+def getsecrectnumber():
     numbers = list(range(0, 9))
     random.shuffle(numbers)
     num_tobeguessed = ''
     for i in range(num_digits):
         num_tobeguessed += str(numbers[i])
     return num_tobeguessed
+
+def clues():
+
