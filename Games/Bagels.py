e92e72770b905e1411a28bae15b1c831a60649f9
SoultySprings <tanishqchakravarty@gmail.com>
2025-05-16 12:43:25 +0530

    commit 160525

--- Games/Bagels.py
+++ Games/Bagels.py
@@ -1,26 +1,33 @@
 #code to deduce a secret 3-digit number when given max 20 guesses
+import numbers
 import random
 
-num_digits = 3
-max_guess = 20
-
 def main():
+    num_digits = 3
+    max_guess = 20
     print('''The following game allows the user to deduce a three digit random integer.
     the rules simply says that:
     1. if a digit is in the right place - Fermi
     2. if digit in the wrong place but exists in number - Gamma
     3. if digit not in number - Bagels
     ''')
-    guessing_number = getsecrectnumber()
+    guessing_number = getsecrectnumber(num_digits)
 
     while True:
         print(f'You have {max_guess} guesses in total.)')
-        print('''The number has been guessed''')
-        guess = 1
-        while guess <= max_guess :
+        print('''The number has been imagined.''')
+        guesscount = 1
+        while guesscount <= max_guess :
+            user_num = str(input('Enter your guess: '))
+            clues(user_num, guessing_number, num_digits)
+        if user_num == guessing_number:
+            break
+        elif guesscount > max_guess:
+            print('You have ran out of guesses!')
+            break
+        guesscount += 1
 
-
-def getsecrectnumber():
+def getsecrectnumber(num_digits):
     numbers = list(range(0, 9))
     random.shuffle(numbers)
     num_tobeguessed = ''
@@ -28,5 +35,14 @@ def getsecrectnumber():
         num_tobeguessed += str(numbers[i])
     return num_tobeguessed
 
-def clues():
+def clues(user_num, guessing_number,num_digits):
+    if user_num == guessing_number:
+        print('You have guessed the correct number!')
+    for i in range(0, num_digits):
+        if user_num[i] == guessing_number[i]:
+            print('Fermi')
+        elif user_num[i] in guessing_number:
+            print('Gamma')
+        elif user_num[i] != guessing_number:
+            print('Bagels')
 
