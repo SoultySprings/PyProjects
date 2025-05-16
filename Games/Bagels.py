26f0d7ebb5fdf1dd14e995cc1f2321dadd55d00b
SoultySprings <tanishqchakravarty@gmail.com>
2025-05-16 12:22:02 +0530

    commit 160525

--- Games/Bagels.py
+++ Games/Bagels.py
@@ -1,3 +1,13 @@
-#code to deduce a secret 3-digit number
+#code to deduce a secret 3-digit number when given max 20 guesses
 import random
 
+num_digits = 3
+max_guess = 20
+
+def getsecrectnumer:
+    numbers = list(range(0, 9))
+    random.shuffle(numbers)
+    num_tobeguessed = ''
+    for i in range(num_digits):
+        num_tobeguessed += str(numbers[i])
+    return num_tobeguessed
