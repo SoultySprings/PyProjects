8a18efa893feaf3b00a0a950de62f7ba35c84aea
SoultySprings <tanishqchakravarty@gmail.com>
2025-05-16 13:04:02 +0530

    commit 160525

--- Games/Bagels.py
+++ Games/Bagels.py
@@ -5,27 +5,40 @@ import random
 def main():
     num_digits = 3
     max_guess = 20
-    print('''The following game allows the user to deduce a three digit random integer.
+    print('''
+    
+    The following game allows the user to deduce a three digit random integer.
     the rules simply says that:
     1. if a digit is in the right place - Fermi
     2. if digit in the wrong place but exists in number - Gamma
     3. if digit not in number - Bagels
+    
     ''')
     guessing_number = getsecrectnumber(num_digits)
 
     while True:
-        print(f'You have {max_guess} guesses in total.)')
+        print(f'You have {max_guess} guesses in total.')
         print('''The number has been imagined.''')
         guesscount = 1
         while guesscount <= max_guess :
-            user_num = str(input('Enter your guess: '))
-            clues(user_num, guessing_number, num_digits)
-        if user_num == guessing_number:
+            user_num = str(input(f'Guess number {guesscount}. Enter your guess: '))
+            if len(user_num) == num_digits and user_num.isdigit():
+                clues(user_num, guessing_number, num_digits)
+                if user_num == guessing_number:
+                    print('You have guessed the correct number!')
+                    break
+                elif guesscount > max_guess:
+                    print('You have ran out of guesses!')
+                    break
+                guesscount += 1
+            else: print('The input is incorrect, please try again')
+
+        reply = input("Do you wish to play again or quit? Y for Yes or N for No : ")
+        if reply.lower() == 'y':
+            print('Okay, all the best!')
+        elif reply.lower() == 'n':
+            print('Thank you for playing!')
             break
-        elif guesscount > max_guess:
-            print('You have ran out of guesses!')
-            break
-        guesscount += 1
 
 def getsecrectnumber(num_digits):
     numbers = list(range(0, 9))
@@ -36,8 +49,6 @@ def getsecrectnumber(num_digits):
     return num_tobeguessed
 
 def clues(user_num, guessing_number,num_digits):
-    if user_num == guessing_number:
-        print('You have guessed the correct number!')
     for i in range(0, num_digits):
         if user_num[i] == guessing_number[i]:
             print('Fermi')
@@ -46,3 +57,4 @@ def clues(user_num, guessing_number,num_digits):
         elif user_num[i] != guessing_number:
             print('Bagels')
 
+main()
