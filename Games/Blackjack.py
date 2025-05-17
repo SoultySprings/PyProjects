284839278e2ddcf7b999779fdd5174621b01e3fb
SoultySprings <tanishqchakravarty@gmail.com>
2025-05-17 15:35:30 +0530

    commit 170525

---
+++ Games/Blackjack.py
@@ -0,0 +1,14 @@
+def main:
+    print("The game of Blackjack remade:")
+    print('''The rules are simply that:
+    1. You deal a number
+    2. The dealer then deals a number
+    3. Based on that and the number that you have, you have 2 choices
+    4. You either Hit, to get a larger number but below 21, or
+    5. You either Stay, to have the same number and let the Dealer Hit
+    6. If the number goes above 21 for you, you lose
+    7. If after staying, the dealer deals a higher number you lose,
+    8. If after hitting or staying, ur number is higher than then the dealer but below 21, you win!
+    
+    Shall we start the game?''')
+
