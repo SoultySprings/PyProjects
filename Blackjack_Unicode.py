2f91fbd8835da8e03bb7bcfd3a09791d976f442e
SoultySprings <tanishqchakravarty@gmail.com>
2025-05-18 19:43:55 +0530

    commit 170525

---
+++ Blackjack_Unicode.py
@@ -0,0 +1,19 @@
+'''Blackjack using unicode, a different approach that includes money as well
+Rules are the same:
+1. Hit 21 or above the dealers bet or else you lose
+2. Play till you have money or else you lose
+'''
+
+import random, sys
+
+hearts = chr(9829)
+diamonds = chr(9830)
+spades = chr(9824)
+clubs = chr(9827)
+
+print(f'''
+Hearts      :   {hearts}    
+Diamonds    :   {diamonds}
+Spades      :   {spades}       
+Clubs       :   {clubs}''')
+
