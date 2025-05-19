05eafdb29e88eb18f868818b70fd55a23bbe1f55
SoultySprings <tanishqchakravarty@gmail.com>
2025-05-19 12:36:07 +0530

    commit 190525

--- Games/BluffMaster.py
+++ Games/BluffMaster.py
@@ -5,36 +5,101 @@ import numpy as np, random, sys
 
 numberOfDices = 5
 numberOfPlayers = 2
+#BACKSIDE = backside
+diceCircle = chr(9679)
+diceDict = {
+0: f''' _______ 
+|???????| 
+|???????| 
+|_______| 
+''',
+1:f''' _______ 
+|       | 
+|   {diceCircle}   | 
+|_______| 
+''',
+2:f''' _______ 
+|{diceCircle}      | 
+|       | 
+|______{diceCircle}| 
+''',
+3:f''' _______ 
+|{diceCircle}      | 
+|   {diceCircle}   | 
+|______{diceCircle}| 
+''',
+4:f''' _______ 
+|{diceCircle}     {diceCircle}| 
+|       | 
+|{diceCircle}_____{diceCircle}| 
+''',
+5:f''' _______ 
+|{diceCircle}     {diceCircle}| 
+|   {diceCircle}   | 
+|{diceCircle}_____{diceCircle}| 
+''',
+6:f''' _______ 
+|{diceCircle}     {diceCircle}| 
+|{diceCircle}     {diceCircle}| 
+|{diceCircle}_____{diceCircle}| 
+'''}
+
 
 def main() :
-    player_name1 = input("Enter the name of player 1 : ")
-    player_name2 = input("Enter the name of player 2 : ")
-    players = [player_name1, player_name2]
-    playerCash = np.array([])
-    playerDices = np.array([], dtype=np.int8)
-    for player in players :
-        money = getBet();
-        print(f"Bet money of {player} is : ${money}")
-        playerCash += [money]
-    print(playerCash)
-    playerDices = getDices()
-
-    #playerDices = playerDices.reshape((2,5))
-    #for i in range(1,2):
-    #    print(playerDices.)
-
-
-
-def getBet() :
-    bet = int(input("Enter the amount you want to bet : "))
-    return bet
-
-def getDices():
-    userDices = np.random.randint(1,6,size=(numberOfPlayers, numberOfDices))
-    print(userDices)
-    return userDices
-
-
+    print(diceDict[0])
+    print(diceDict[1])
+    print(diceDict[2])
+    print(diceDict[3])
+    print(diceDict[4])
+    print(diceDict[5])
+    print(diceDict[6])
+#     player_name1 = input("Enter the name of player 1 : ")
+#     player_name2 = input("Enter the name of player 2 : ")
+#     players = [player_name1, player_name2]
+#     playerCash = np.array([])
+#     playerDices = np.array([], dtype=np.int8)
+#
+#
+#     for player in players :
+#         money = getBet();
+#         print(f"Bet money of {player} is : ${money}")
+#         playerCash += [money]
+#     print(playerCash)
+#     playerDices = getDices()
+#     playerNum = 0
+#     for player in players :
+#         showDices(players, playerDices, False, playerNum)
+#         playerNum += 1
+#
+#     #playerDices = playerDices.reshape((2,5))
+#     #for i in range(1,2):
+#     #    print(playerDices.)
+#
+#
+#
+# def getBet() :
+#     bet = int(input("Enter the amount you want to bet : "))
+#     return bet
+#
+# def getDices():
+#     userDices = np.random.randint(1,6,size=(numberOfPlayers, numberOfDices))
+#     print(userDices)
+#     return userDices
+#
+# def showDices(playerNames, playerDices, showOpponentHand, playerNumber) :
+#     if showOpponentHand == False :
+#         print(" ___ ")
+#         print("|###|")
+#         print("|###|")
+#         print("|###|")
+#         print("|___|")
+#     else:
+#         print("You have the following dices :")
+#         print(" ___ ")
+#         print("|")
+#         print("___")
+#         print("___")
+#         print("___")
 
 if __name__ == '__main__':
     main()
