0a03f9b963706f274f60182ca386f17aeceb919c
SoultySprings <tanishqchakravarty@gmail.com>
2025-05-24 13:52:28 +0530

    commit 240525

--- DSAinPython/binarySearch.py
+++ DSAinPython/binarySearch.py
@@ -1,20 +1,29 @@
 # This is a code for the Binary Search algorithm in Python
+import random as rd
 
 def binarySearch(array, requiredItem):
     low, high = 0, len(array)-1
+    print(f"Array is : {array} and required element is : {requiredItem}.")
     while low <= high:
         mid = (low + high) // 2
         midNumber = array[mid]
-        print(f"Low : {low}, High : {high}, Mid : {mid}, Mid Number : {midNumber}")
         if midNumber == requiredItem:
-            return mid
+            return f"Element at index {mid}."
         elif midNumber > requiredItem:
             high = mid - 1
         elif midNumber < requiredItem:
             low = mid + 1
-    return -1
+    return "Element not found!"
 
 if __name__ == '__main__':
-    print(binarySearch([1, 2, 3, 4, 5, 6], 5))
+    array = []
+    i = 0
+    while i<20:
+        array.append(rd.randint(1, 500))
+        i += 1
+    requestedItem = rd.choice(array)
+    array.sort()
+    print(array, requestedItem)
+    print(binarySearch(array, requestedItem))
 
 
