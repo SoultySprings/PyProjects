5a5047d94fe24bf66d45b94c9dd2570a779df851
Tanishq <132868290+SoultySprings@users.noreply.github.com>
2025-05-28 18:41:49 +0530

    commit 280525

---
+++ DSAinPython/OtherExamples/meanAvgNums.py
@@ -0,0 +1,21 @@
+#This code has been taken from the book The Common Sense Guide to DSA (in short TCSG to DSA)
+#In short calculates the mean of even, odd and then overall numbers present in the array
+
+def averageEvenOddOverall(array):
+    sumEven, sumOdd, sumOverall = 0,0,0
+    countofEvenNums, countofOddNums, countofOverallNums = 0,0,0
+    for number in array:
+        if number % 2 == 0 :
+            countofEvenNums += 1
+            sumEven += number
+        elif number % 2 == 1:
+            countofOddNums += 1
+            sumOdd += number
+        sumOverall += number
+    return sumEven//countofEvenNums, sumOdd//countofOddNums, sumOverall//len(array)
+
+if __name__ == '__main__' :
+    array = list(range(1,100))
+    print(f'The given array is : {array}')
+    aEven, aOdd, aOver = averageEvenOddOverall(array)
+    print(f'Average of even : {aEven}, of odd : {aOdd}, and overall : {aOver}.')
