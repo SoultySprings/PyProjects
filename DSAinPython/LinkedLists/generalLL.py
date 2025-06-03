2694d16a8d46f92254c9a5efe79cde03722d9854
Tanishq <132868290+SoultySprings@users.noreply.github.com>
2025-06-03 14:36:08 +0530

    commit 030625

--- DSAinPython/LinkedLists/generalLL.py
+++ DSAinPython/LinkedLists/generalLL.py
@@ -1,4 +1,34 @@
 import basicNode
 class LinkedList:
-    def __init__(self, first_node = None):
-        self.first_node = first_node
+    def __init__(self, firstNode = None):
+        self.firstNode = firstNode
+
+    def read(self, index):
+        currentNode = self.firstNode
+        currentIndex = 0
+
+        while currentIndex < index:
+            currentNode = currentNode.nextNode
+            currentIndex += 1
+
+            if not currentNode:
+                return None
+
+        return currentNode.data
+
+    def search(self, value):
+        currentNode = self.firstNode
+        currentIndex = 0
+
+        while True:
+            if currentNode == value:
+                return currentIndex
+            currentNode = currentNode.nextNode
+
+            if not currentNode:
+                break
+
+            currentIndex += 1
+        return None
+
+
