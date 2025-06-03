7bf7cd4fd5105cc205247f6c9648c6288e8e5b83
Tanishq <132868290+SoultySprings@users.noreply.github.com>
2025-06-03 12:47:52 +0530

    commit 030525

---
+++ DSAinPython/LinkedLists/basicNode.py
@@ -0,0 +1,8 @@
+#This code has been taken from A CSG to DSA
+#The following is the basic code for a linked list
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next_node = None
+
