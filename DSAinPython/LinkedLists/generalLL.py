5837cb2c57aef9aeaf4d45e46b3be7a9060db6f5
Tanishq <132868290+SoultySprings@users.noreply.github.com>
2025-06-03 14:41:14 +0530

    commit 030625

--- DSAinPython/LinkedLists/generalLL.py
+++ DSAinPython/LinkedLists/generalLL.py
@@ -31,4 +31,34 @@ class LinkedList:
             currentIndex += 1
         return None
 
+    def insert(self, index, value):
+        newNode = basicNode.Node(value)
 
+        if index == 0:
+            newNode.nextNode = self.firstNode
+            self.firstNode = newNode
+            return
+
+        currentNode = self.firstNode
+        currentIndex = 0
+
+        while currentIndex < (index - 1):
+            currentNode = currentNode.nextNode
+            currentIndex += 1
+
+        newNode.nextNode = currentNode.nextNode
+        currentNode.nextNode = newNode
+
+    def delete(self, index):
+        if index == 0 :
+            self.firstNode = self.firstNode.nextNode
+            return
+        currentNode  = self.firstNode
+        currentIndex = 0
+        while currentIndex < (index - 1):
+            currentNode  = currentNode.nextNode
+            currentIndex +=1
+
+        nodeAfterDeletedNode = currentNode.nextNode.nextNode
+        currentNode.nextNode = nodeAfterDeletedNode
+        
